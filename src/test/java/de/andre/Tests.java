ad02e49a6c7bcdb24d8cc68f6d2c25b6649ed8c5
andrei.ivanov <andrey.evans@gmail.com>
2015-09-27 19:24:32 +0300

    improvs..

--- src/test/java/de/andre/Tests.java
+++ src/test/java/de/andre/Tests.java
@@ -5,13 +5,18 @@ import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.beans.PropertyValue;
 
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.List;
 
 /**
  * Created by andreika on 8/6/2015.
  */
 public class Tests {
 	public static void main(String[] args) {
+		for (String str : getList()) {
+			System.out.println(str);
+		}
 
 		Calendar calendar = Calendar.getInstance();
 		calendar.set(Calendar.YEAR, calendar.get(Calendar.YEAR) - 10);
@@ -23,5 +28,12 @@ public class Tests {
 		System.out.println(person.getEmail());
 		System.out.println(person.getLastName());
 		System.out.println(beanWrapper.getPropertyValue("email"));
+	}
+
+	public static final List<String> list = Arrays.asList("1", "2", "3");
+
+	public static List<String> getList() {
+		System.out.println("ga");
+		return list;
 	}
 }
