76c33f985d71cc4ff07cbd98fc4564168a870316
andrei.ivanov <andrey.evans@gmail.com>
2016-05-04 23:00:58 +0300

    catalog rework

--- src/main/java/de/andre/service/catalog/CatalogTools.java
+++ src/main/java/de/andre/service/catalog/CatalogTools.java
@@ -11,7 +11,9 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
+import java.time.LocalDateTime;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class CatalogTools {
     private static final Logger logger = LoggerFactory.getLogger(CatalogTools.class);
@@ -38,7 +40,7 @@ public class CatalogTools {
         final String filterCatId = getFilterCategoryId(cat, parentCat);
         final List<Product> allProducts = getProductsWithAncestorsCat(cat);
 
-        final Map<String, String> categoryIdNameMap = getSubcategoriesNames(filterCatId);
+        final Map<String, String> categoryIdNameMap = getSubcategoriesNames(cat, parentCat, filterCatId);
         if (categoryIdNameMap == null || categoryIdNameMap.isEmpty()) {
             logger.warn("No subcategories found for category {}, returning without processing", catId);
         }
@@ -52,15 +54,25 @@ public class CatalogTools {
     }
 
     private List<Product> getProductsWithAncestorsCat(final Category rootCategory) {
+        // let's try to find and filter products in memory
         final List<Product> resultList = new ArrayList<>(30);
         final Set<String> seenSoFar = new HashSet<>();
 
         processCategory(rootCategory, seenSoFar, resultList);
 
-        if (!resultList.isEmpty()) {
-            Collections.sort(resultList);
-        }
-        return resultList;
+        final LocalDateTime now = LocalDateTime.now();
+        final String siteId = SiteManager.getSiteId();
+        logger.trace("Filtering {} products by {} date and {} site",
+                     resultList.size(), now.toString(), siteId);
+
+        return resultList
+                .stream()
+//                .filter(prd -> prd contains(siteId))
+                .filter(prd -> prd.isEnabled() && (prd.getExpirationDate() == null || prd.getExpirationDate().isAfter(now)) &&
+                        prd.getStartDate() == null || prd.getStartDate().isBefore(now))
+                .distinct()
+                .sorted()
+                .collect(Collectors.toList());
     }
 
     private void processCategory(final Category category, final Set<String> seenSoFar,
@@ -93,21 +105,31 @@ public class CatalogTools {
         return category.getId();
     }
 
-    private Map<String, String> getSubcategoriesNames(final String catId) {
-        final List<String[]> childCategories = productCatalog.getCatalogRepository()
-                .getSubCategoriesIdsAndNames(catId);
+    private Map<String, String> getSubcategoriesNames(final Category baseCategory, final Category parentCategory,
+            final String filterCatId) {
+        final Set<Category> subcategories;
+        if (baseCategory.getId().equals(filterCatId)) {
+            logger.debug("Using base category: {}", baseCategory.getId());
+            subcategories = baseCategory.getChildCategories();
+        } else {
+            logger.debug("Using parent category: {}", parentCategory.getId());
+            subcategories = parentCategory.getChildCategories();
+        }
 
-        if (childCategories.isEmpty()) {
-            logger.debug("No child categories found for category: {}", catId);
+        if (subcategories.isEmpty()) {
+            logger.debug("No child categories found for category: {}", filterCatId);
             return Collections.emptyMap();
         }
 
-        final Map<String, String> categoryIdNameMap = new LinkedHashMap<>(childCategories.size());
-        for (final Object[] category : childCategories) {
-            categoryIdNameMap.put(
-                    String.valueOf(category[0]), String.valueOf(category[1]));
-        }
-        return categoryIdNameMap;
+        return subcategories
+                .stream()
+                .sorted(Comparator.comparing(Category::getDisplayName))
+                .collect(Collectors.toMap(
+                        Category::getId,
+                        Category::getDisplayName,
+                        (v1, v2) -> v1,
+                        LinkedHashMap::new)
+                );
     }
 
     @Transactional(readOnly = true)
