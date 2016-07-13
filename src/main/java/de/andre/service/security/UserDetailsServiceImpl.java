a90202bd0be74736a9a97d7bb66f9a5a4eb198c2
andrei.ivanov <andrei.ivanov@objectedge.com>
2016-07-13 18:31:44 +0300

    config changes

--- src/main/java/de/andre/service/security/UserDetailsServiceImpl.java
+++ src/main/java/de/andre/service/security/UserDetailsServiceImpl.java
@@ -4,22 +4,22 @@ import de.andre.entity.profile.Profile;
 import de.andre.service.account.ProfileTools;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 
-@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
     private static final Logger logger = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
 
-    @Autowired
-    ProfileTools accountTools;
+    private final ProfileTools profileTools;
+
+    public UserDetailsServiceImpl(final ProfileTools profileTools) {
+        this.profileTools = profileTools;
+    }
 
     @Override
     public UserDetails loadUserByUsername(final String pEmail) throws UsernameNotFoundException {
-        final Profile profile = accountTools.profileByEmail(pEmail)
+        final Profile profile = profileTools.profileByEmail(pEmail)
                 .orElseThrow(() -> new UsernameNotFoundException("Profile wasn\'t found with the email: " + pEmail));
 
         if (logger.isDebugEnabled()) {
